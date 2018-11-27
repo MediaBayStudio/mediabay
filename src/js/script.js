4623d7c621c6efee1d068a9af5232c9783d1cd23
MediaBayStudio <med.bay@yandex.ru>
2018-11-27 19:26:27 +0300

    Начали разметку. Хэдер на мобильном. Первый слайд с квадратами. Начали что мы делаем

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,143 @@
 svg4everybody(); // иницализация полифила для IE
+(function($, window) {
+  var _defaults = {
+    x: 2, // number of tiles in x axis
+    y: 2, // number of tiles in y axis
+    random: true, // animate tiles in random order
+    speed: 2000 // time to clear all times
+  };
 
-$(document).ready(function(){
-  // весь ваш код c jQuery
+  function range(min, max, rand) {
+    var arr = new Array(++max - min)
+      .join(".")
+      .split(".")
+      .map(function(v, i) {
+        return min + i;
+      });
+    return rand
+      ? arr
+          .map(function(v) {
+            return [Math.random(), v];
+          })
+          .sort()
+          .map(function(v) {
+            return v[1];
+          })
+      : arr;
+  }
+
+  function randomInteger(min, max) {
+    var rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;
+  }
+
+
+
+  // Prevent css3 transitions on load
+  $("body").addClass("css3-preload");
+  $(window).load(function() {
+    $("body").removeClass("css3-preload");
+  });
+
+  $.fn.sliced = function(options) {
+    var o = $.extend({}, _defaults, options);
+    return this.each(function() {
+      var $container = $(this);
+
+      /*---------------------------------
+       * Make the tiles:
+       ---------------------------------*/
+      var $img = $container.find("img"),
+        width = $img.width(),
+        height = $img.height(),
+        n_tiles = o.x * o.y,
+        tiles = [],
+        $tiles;
+
+      for (var i = 0; i < n_tiles; i++) {
+        tiles.push('<div class="tile"/>');
+      }
+
+      $tiles = $(tiles.join(""));
+      $img.after($tiles);
+      $tiles.css({
+        width: width / o.x,
+        height: width / o.x,
+        backgroundColor: "rgba(255,255,255,0.5)"
+      });
+
+      var $dump = $tiles; //дамп нужен, чтобы посчитать позиционирование, не вырывая блок из потока
+      var offset_x = 0;
+
+      // Adjust position
+      $dump.each(function(i) {
+        var pos = $(this).position();
+        if (i == 0) {
+          offset_x = pos.top-height; // убираем погрешность между высотами
+        }
+        $(this).css("top", pos.top-height-offset_x + "px ");
+        $(this).css("left", pos.left + "px ");
+        $tiles[i] = $(this);
+      });
+
+      $tiles.each(function(i) {
+        $(this).css("position", 'absolute');
+      });
+
+
+      /*---------------------------------
+       * Animate the tiles:
+       ---------------------------------*/
+      // Public method
+      $container.on("start_quot", function() {
+        setInterval(function() {
+          var i = randomInteger(0, 48);
+          var num = 0; // считаем, сколько квадратов у нас всего
+          var elems = []; // сохраняем видимые квадраты
+           for (var n = 0; n<48; n++) {
+             if( $tiles[n].hasClass('tile-animated') ) {
+               num = num+1;
+               elems.push(n);
+             }
+           }
+          if (num >= 10) {
+            var rand = Math.floor(Math.random() * elems.length);
+            $tiles.eq(elems[rand]).toggleClass("tile-animated");
+          }
+          else {
+            $tiles.eq(i).toggleClass("tile-animated");
+          }
+        }, 1000);
+      });
+    });
+  };
+})(jQuery, window);
+
+$(window).load(function() {
+  var waitForFinalEvent = (function () {
+    var timers = {};
+    return function (callback, ms, uniqueId) {
+      if (!uniqueId) {
+        uniqueId = "Don't call this twice without a uniqueId";
+      }
+      if (timers[uniqueId]) {
+        clearTimeout (timers[uniqueId]);
+      }
+      timers[uniqueId] = setTimeout(callback, ms);
+    };
+  })();
+
+  $(window).resize(function () {
+    waitForFinalEvent(function(){
+
+      $("div.tile").remove();
+      $(".slider__sliced").sliced({ x: 8, y: 6, speed: 5500 });
+      $(".slider__sliced").trigger("start_quot");
+    }, 500);
+  });
+
+  $(".slider__sliced").sliced({ x: 8, y: 6, speed: 5500 });
+  $(".slider__sliced").trigger("start_quot");
+
 });
-
-// Если на проекте нет jQuery, но хочется $( document ).ready... (IE9+)
-// function ready(fn) {
-//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-//     fn();
-//   } else {
-//     document.addEventListener('DOMContentLoaded', fn);
-//   }
-// }
-//
-// ready(function(){
-//   // code
-// });
